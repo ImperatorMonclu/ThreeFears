532216e28ec22cde47fbaa106030104b230fb98e
MauroGarciaMonclu <maurogarciamonclu@gmail.com>
2020-07-10 19:40:34 +0200

    Inicialización del proyecto, Iteración 1 completada y Iteración 2 empezada.

---
+++ Project/Source/ThreeFears/ThreeFears.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThreeFears.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThreeFears, "ThreeFears" );
---
+++ Project/Source/ThreeFears/ThreeFearsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThreeFearsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREEFEARS_API AThreeFearsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
